2a0207525e2c72f16899180ccf927737c83ff051
Felix Schmidt <para.feivl.dox@gmail.com>
2023-10-23 14:31:29 +0200

    First task completed. Created add activities form.

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -1,5 +1,61 @@
 import "./Form.css";
 
-export default function Form() {
-	return <h1>Test</h1>;
+export default function Form({ onAddActivity }) {
+	function handleSubmit(event) {
+		event.preventDefault();
+		const formData = new FormData(event.target);
+		const data = Object.fromEntries(formData);
+
+		const activityName = data.activityNameInput;
+		const isForGoodWeather = data.weatherCheckbox ? true : false;
+
+		onAddActivity({
+			name: activityName,
+			isForGoodWeather: isForGoodWeather,
+		});
+
+		event.target.reset();
+		event.target.elements.activityNameInput.focus();
+	}
+
+	return (
+		<>
+			<h2>Add new Activity:</h2>
+			<form className="form" onSubmit={handleSubmit}>
+				{/* for="nameInput" ist der Bezug zu dem Input (Barrierefreiheit)   */}
+				<fieldset>
+					<fieldset className="form--fieldset">
+						<label
+							className="nameInput"
+							htmlFor="activityNameInput"
+						>
+							Name:
+						</label>
+						<input
+							id="activityNameInput"
+							type="text"
+							name="activityNameInput"
+							required
+						/>
+					</fieldset>
+					<fieldset className="form--fieldset">
+						<label
+							className="weather--checkbox"
+							htmlFor="weatherCheckbox"
+						>
+							Good-weather activity:
+						</label>
+						<input
+							id="weatherCheckbox"
+							name="weatherCheckbox"
+							type="checkbox"
+						/>
+					</fieldset>
+					<button type="submit" className="form--submit">
+						Submit
+					</button>
+				</fieldset>
+			</form>
+		</>
+	);
 }
